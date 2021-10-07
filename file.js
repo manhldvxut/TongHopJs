a1b25350962375a5e3abfba9bd9b2e3b6685666d
dh_lemanh <lemanh@demo.co.jp>
2021-10-07 17:45:57 +0900

    check input katakana

--- file.js
+++ file.js
@@ -14,3 +14,52 @@ $(document).ready(function(linkOtherPage) {
 } );
 
 
+// check input katakana
+
+var changerColor = {backgroundColor: '#FCF3F3', borderColor : '#F2C2CA'};
+var returnColor = {backgroundColor: '#fff', borderColor : '#D3D3CF'};
+var textSelected = '選択してください';
+
+$('.required__check').each(function(){
+    const currentCheck = $(this)
+    console.log(currentCheck.val())
+
+    $(currentCheck).on('keydown keyup keypress change focus blur', function() {
+        
+        if ($(this).val() == '') {
+            $(this).css(changerColor);
+        } 
+        else {
+            $(this).css(returnColor);
+        }
+
+    }).change();
+
+})
+
+var valueSlection = $( ".sel-opt option:selected" ).val()
+
+if(valueSlection == textSelected){
+    $('.sel-opt').css(changerColor);
+}else{
+    $('.sel-opt').css(returnColor);
+}
+
+$('.sel-opt').change(function(){
+
+    if($(this).val() != textSelected){
+        $(this).css(returnColor);
+    }else{
+        $(this).css(changerColor);
+    }
+})
+
+// check katakana
+$('.checkKatakana').on('keydown keyup keypress change focus blur', function() {
+    const checktextKatanata = $(this).val();
+    if(checktextKatanata.match(/^[ァ-ヶー　]+$/) || checktextKatanata == '' ){
+        $(this).css(returnColor)
+      }else{
+        $(this).css(changerColor)
+      }
+}).change()
